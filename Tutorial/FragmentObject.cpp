043ad6861ca002fd368401ba33a0d42511564110
luchoscar <losh177@hotmail.com>
2013-12-30 23:32:10 -0800

    Files for FS Object

---
+++ Tutorial/FragmentObject.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "FragmentObject.h"
+
+
+FragmentObject::FragmentObject()
+{
+}
+
+
+FragmentObject::~FragmentObject()
+{
+}
---
+++ Tutorial/FragmentObject.h
@@ -0,0 +1,8 @@
+#pragma once
+class FragmentObject
+{
+public:
+	FragmentObject();
+	virtual ~FragmentObject();
+};
+
